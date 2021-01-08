a90a258b128a2d1ac03ce15d5f979c3522dca9d5
CPC <www.chenpengchong@gmail.com>
2021-01-08 10:58:35 +0800

    新增文件

---
+++ 04_布尔值和空值.py
@@ -0,0 +1,15 @@
+# 布尔值(boolean bool)和空值
+# 布尔值主要用来做逻辑判断
+# 布尔值一共有俩个,一个是True 和 False
+a = True
+b = False
+print(a)
+print(b)
+# 布尔值实际上也属于整形,True就相当于1,False就相当于0
+print(1 + True)
+print(1 + False)
+
+# None (空值)
+# None专门用来表示不存在
+c = None
+print(c)
---
+++ 05_类型检查.py
@@ -0,0 +1,19 @@
+# 类型检查
+# 数值
+#    整形
+#       布尔值
+#    浮点型
+#    复数
+#  字符串
+#  空值
+# type() 用来检查值的类型
+a = 123
+b = '123'
+print(type(a))
+print(type(b))
+
+print(type(1))
+print(type(1.5))
+print(type(True))
+print(type('hello'))
+print(type(None))
