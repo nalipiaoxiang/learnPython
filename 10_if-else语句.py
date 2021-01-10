de3510860f41c5f21ccd71c11113942491fe9650
CPC <www.chenpengchong@gmail.com>
2021-01-10 18:54:45 +0800

    新增文件

---
+++ 10_if-else语句.py
@@ -0,0 +1,11 @@
+# if-else语句
+# 语法:
+#   if 条件表达式:
+#        代码块
+#   else :
+#        代码块
+age = 16
+if age > 17 :
+    print('你已经成年了')
+else:
+    print('你还未成年')
---
+++ 11_if-elif-else语句.py
@@ -0,0 +1,22 @@
+# if-elif-else语句
+# 语句:
+#   if 条件表达式:
+#       代码块
+#   elif 条件表达式:
+#       代码块
+#   elif 条件表达式:
+#       代码块
+#   .......
+#   else:
+#       代码块
+age = 17
+if age > 200 :
+    print('活够了')
+elif age > 100 :
+    print('活的挺久的')
+elif age > 60 :
+    print('退休了')
+elif age >= 18 :
+    print('成年了')
+else:
+    print('你还是小孩')
